cecce6565c4a151e7dca9bfe6646e711ddc29059
Sarthak0402 <dsarthak12@gmail.com>
2022-12-24 17:54:07 +0530

    sms-dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {FaApple, FaGoogleDrive} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineMail } from "react-icons/ai";
+import { useEffect } from 'react';
+
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      This is Navbar  
       </header>
+      <div className='container'>
+      <div className='LoginForm'>
+        <h1 className='login'> Login</h1>
+        <div className='inputs'>
+        <div>
+        <h4>Username/ID</h4>
+        <input type='text' placeholder='yourmail@gmail.com' className='User'></input>
+        </div>
+        <div className='user'>
+        <h4>Password</h4>
+        <input type='password' placeholder='Password' className='User'>
+        </input>
+        </div>
+        <div className='socialmedia'>
+          <div className='Apple'>
+          <button className='btnn'>
+          <FaApple className='apple-icon' />
+          </button>
+          </div>
+          <div className='Facebook'>
+            <button className='btnn'>
+            <FaFacebook className='fb-icon' />
+            </button>
+          </div>
+          <div className='Google'>
+            <button className='btnn'>
+            <FcGoogle className='google-icon' />
+            </button>
+          </div>
+        </div>
+        <button className='btn-login'>Login</button>
+        <div className='createacc'>
+          Don't have account? 
+          <a href='#'>Create Account <br /><br /></a>
+          If you are an admin, you can register yourself right away<br />. If you are a student, ask your admin to register  
+        </div>
+        </div>
+        
+      </div>
+      </div>
     </div>
   );
 }
